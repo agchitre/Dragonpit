9c262a1a745d30ea44e06b339190036e89801b1e
agchitre <106405125+agchitre@users.noreply.github.com>
2022-12-12 12:23:21 -0800

    index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const height = 200;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
 var roar = new Audio('roar.mp3');
-//document.getElementById("image").style.visibility = "hidden"
+document.getElementById("image").style.visibility = "hidden"
 
 
 let img = new Image();
