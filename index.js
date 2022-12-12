4f19d806cdc2cc5a084854e1ab9b7a5f8581b959
agchitre <106405125+agchitre@users.noreply.github.com>
2022-12-12 11:48:37 -0800

    index js

--- index.js
+++ index.js
@@ -148,7 +148,9 @@ const messageSentCallback = function(result) {
 };
 
 function dragonroar() {  
-    roar.play();  
+    roar.play(); 
+    animateScript(); 
+    stopAnimate();
 }
 
 /**
@@ -207,3 +209,37 @@ bindButton('micButton', () => {
         }
     })
 });
+
+//FIRE THROW
+var tID; //we will use this variable to clear the setInterval()
+
+function stopAnimate() {
+  clearInterval(tID);
+  document.getElementById("image").style.visibility = 'hidden'; 
+
+} //end of stopAnimate()
+
+
+function animateScript() {
+  var position = 256; //start position for the image slicer
+  const interval = 100; //100 ms of interval for the setInterval()
+  const diff = 256; //diff as a variable for position offset
+  document.getElementById("image").style.visibility = 'visible'; 
+  tID = setInterval(() => {
+  
+    document.getElementById("image").style.backgroundPosition =
+      `-${position}px 0px`;
+    //we use the ES6 template literal to insert the variable "position"
+    
+    if (position < 1536) {
+      position = position + diff;
+    }
+    //we increment the position by 256 each time
+    else {
+      position = 256;
+    }
+    //reset the position to 256px, once position exceeds 1536px
+    
+  }, interval); //end of setInterval
+} //end of animateScript()
+
